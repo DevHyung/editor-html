8e1b5b39c327e51bc11b078287d17a28a076e298
DevHyung <khuphj@gmail.com>
2019-04-06 21:16:16 +0900

    Release 190406 21:15

---
+++ db/create_post.php
@@ -0,0 +1,36 @@
+<?php
+require_once("./dbconfig.php");
+if (!isset($_SESSION)) {
+    session_start();
+}
+$id = $_SESSION['id'];
+$title = $_POST['title'];
+$writer = $_SESSION['auth'];
+$content = $_POST['content'];
+
+$table_name = "post";
+$sql = "
+    INSERT INTO $table_name (
+        id,
+        title,
+        writer,
+        content,
+        createDateTime,
+        isVerify
+    ) VALUES (
+        '$id',
+        '$title',
+        '$writer',
+        '$content',
+        NOW(),
+        'n'
+    )";
+if ($result = mysqli_query($db, $sql)) {
+    echo "<script>alert('등록되었습니다. ');</script>";
+    echo "<script>window.location.replace('../post_list.php');</script>";
+} else {
+    echo mysqli_error($db);
+}
+mysqli_free_result($result);
+mysqli_close($db);
+?>
